a58004602d3393acb4cd33f9599fbd6fdf958c1b
hendrix <leandrotully@gmail.com>
2023-07-18 08:22:29 -0300

    Multiplas listas

--- script/print/dolar.js
+++ script/print/dolar.js
@@ -2,12 +2,18 @@ const lista = document.getElementById('dolar');
 
 function imprimeCotacao(nome, valor) {
     lista.innerHTML = '';
-
     for (let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
+
+        nome = setMoneyName(multiplicador);
+
         const listaItem = document.createElement('li');
         listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador).toFixed(2)}`
         lista.appendChild(listaItem)
     }
+}
+
+function setMoneyName(value) {
+    return (value == 1) ? 'Dolar' : 'Dolares';
 }
 
 export default imprimeCotacao
--- script/print/iene.js
+++ script/print/iene.js
@@ -4,10 +4,17 @@ function imprimeCotacaoIene(nome, valor) {
     lista.innerHTML = '';
 
     for (let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
+
+        nome = setMoneyName(multiplicador);
+
         const listaItem = document.createElement('li');
         listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador).toFixed(2)}`
         lista.appendChild(listaItem)
     }
+}
+
+function setMoneyName(value) {
+    return (value == 1) ? 'Iene' : 'Ienes';
 }
 
 export default imprimeCotacaoIene
